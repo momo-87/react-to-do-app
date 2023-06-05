3937898b1b63783b240454ee25d32afc53eb831f
Momo-87 <momochristian87@gmail.com>
2023-06-05 11:56:02 +0100

    Rendering the InputTodo and TodosList Components

--- src/components/TodoApp.jsx
+++ src/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import TodoLogic from "@/components/TodoLogic";
+
 const TodoApp = () => {
     return (
       <div>
--- src/components/TodoLogic.jsx
+++ src/components/TodoLogic.jsx
@@ -1,6 +1,29 @@
+import InputTodo from "@/components/InputTodo";
+import TodoList from "@/components/TodoList";
+
 const TodosLogic = () => {
+    const todos = [
+      {
+        id: 1,
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: 2,
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ];
     return (
-      <div>TodosLogic content</div>
-    )
-  }
+        <div>
+            <InputTodo />
+            <TodoList todosProps={todos} />
+        </div>
+    );
+  };
   export default TodosLogic;
